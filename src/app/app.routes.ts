da7822aa5ef6ad7d9cc7833b5d8f69d6749eb3a6
Erick Rocha Nascimento <erickrochaluciano@gmail.com>
2025-11-25 23:29:33 -0300

    Upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CardProdutoComponent } from './components/card-produto/card-produto.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent, title: 'home'},
+    {path: 'card-produto', component: CardProdutoComponent, title: 'CardProduto' },
+    {path: '**', redirectTo: '' }
+];
