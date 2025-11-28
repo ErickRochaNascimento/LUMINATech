62598c0224886ad6dbf1732942db0a9652a607f9
Erick Rocha Nascimento <erickrochaluciano@gmail.com>
2025-11-28 14:50:38 -0300

    10.

--- src/app/pages/productdetail/productdetail.component.ts
+++ src/app/pages/productdetail/productdetail.component.ts
@@ -6,11 +6,12 @@ import { SalesService } from '../../services/sales.service';
 import { Produto } from '../../models/produto.model';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { CardProdutoComponent } from '../../components/card-produto/card-produto.component';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [CommonModule, RouterLink, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterLink, HeaderComponent, FooterComponent, CardProdutoComponent],
   templateUrl: './productdetail.component.html',
   styleUrl: './productdetail.component.css'
 })
@@ -19,23 +20,42 @@ export class ProductDetailComponent implements OnInit {
   private produtoService = inject(CardProdutoService);
   private salesService = inject(SalesService);
 
+  produtosRelacionados: Produto[] = [];
   produto: Produto | null = null;
   loading = true;
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    if (id) {
-      this.produtoService.getProdutoById(id).subscribe({
-        next: (data) => {
-          this.produto = data;
-          this.loading = false;
-        },
-        error: (err) => {
-          console.error(err);
-          this.loading = false;
-        }
-      });
-    }
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('id');
+      if (id) {
+        this.loading = true;
+        this.carregarProduto(id);
+      }
+    });
+  }
+
+  carregarProduto(id: string) {
+    this.produtoService.getProdutoById(id).subscribe({
+      next: (data) => {
+        this.produto = data;
+        this.loading = false;
+        // 4. ASSIM QUE CARREGAR O PRODUTO, BUSCA OS RELACIONADOS PELA CATEGORIA
+        this.carregarRelacionados(data.category);
+      },
+      error: (err) => {
+        console.error(err);
+        this.loading = false;
+      }
+    });
+  }
+
+  carregarRelacionados(categoria: string) {
+    this.produtoService.getProdutos(categoria).subscribe({
+      next: (resp) => {
+        // Filtra para remover o produto que já estamos vendo da lista
+        this.produtosRelacionados = resp.products.filter(p => p.id !== this.produto?.id);
+      }
+    });
   }
 
   comprarProduto() {
