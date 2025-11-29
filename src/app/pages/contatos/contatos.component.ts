9d73e6985dd502b8a92f0c142c7647dbfe574252
Erick Rocha Nascimento <erickrochaluciano@gmail.com>
2025-11-29 18:01:55 -0300

    17

--- src/app/pages/contatos/contatos.component.ts
+++ src/app/pages/contatos/contatos.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FooterComponent } from "../../components/footer/footer.component"; // Importante para o formulário funcionar
 import { HeaderComponent } from '../../components/header/header.component';
-import { SalesDashboardComponent } from "../salesdashboard/salesdashboard.component";
 
 
 @Component({
   selector: 'app-contato',
   standalone: true,
-  imports: [FormsModule, FooterComponent, HeaderComponent, SalesDashboardComponent],
+  imports: [FormsModule, FooterComponent, HeaderComponent],
   templateUrl: './contatos.component.html',
   styleUrl: './contatos.component.css'
 })
